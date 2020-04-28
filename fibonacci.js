4d26b6526794a8530efd78e3a8fbc88de4cafc25
richarduggelberg <62283037+richarduggelberg@users.noreply.github.com>
2020-04-28 18:41:35 +0200

    use just recursive fib

--- fibonacci.js
+++ fibonacci.js
@@ -1,31 +1,9 @@
-function fiboLoop(num){
-  var a = 1, b = 0, temp;
-
-  while (num >= 0){
-    temp = a;
-    a = a + b;
-    b = temp;
-    num--;
-  }
-
-  return b;
-}
-
-function fiboRec(num) {
+function fibo(num) {
   if (num <= 1) return 1;
 
-  return fiboRec(num - 1) + fiboRec(num - 2);
-}
-
-function fiboMemo(num, memo) {
-  memo = memo || {};
-
-  if (memo[num]) return memo[num];
-  if (num <= 1) return 1;
-
-  return memo[num] = fiboMemo(num - 1, memo) + fiboMemo(num - 2, memo);
+  return fib(num - 1) + fib(num - 2);
 }
 
 const num = process.argv.slice(2);
 
-console.log(fiboLoop(num));
+console.log(fib(num));
