e417a8ba80a239ea6608c45a11380d87c8849709
Richard Uggelberg <contact@richarduggelberg.com>
2020-04-29 18:56:02 +0200

    add comment

--- fibonacci.js
+++ fibonacci.js
@@ -4,6 +4,8 @@ function fib(num) {
   return fib(num - 1) + fib(num - 2);
 }
 
+console.log("This is a bunch of text that a coworker added for no real reason");
+
 const num = process.argv.slice(2);
 
 console.log(fib(num));
